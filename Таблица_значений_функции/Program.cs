2d5297265a6f31cd379e5a80970a529d0c9e1e69
Amirka2 <70907990+Amirka2@users.noreply.github.com>
2020-10-01 16:41:44 +0500

    Update Program.cs

--- Таблица_значений_функции/Program.cs
+++ Таблица_значений_функции/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Таблица_значений_функции
 {
@@ -6,7 +6,21 @@ namespace Таблица_значений_функции
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите значение, на которое увеличивается х:");
+            double addition = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите диапазон значений х(начало и конец на разных строках):");
+            int startRange = Convert.ToInt16(Console.ReadLine());
+            int endRange = Convert.ToInt16(Console.ReadLine());
+
+            int i = startRange;
+            double result;
+            while (i <= endRange)
+            {
+                result = i + addition;
+                Console.WriteLine("{0} {1}", i, result);
+                i++;
+            }
         }
     }
 }
