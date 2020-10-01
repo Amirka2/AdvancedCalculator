5ce2314d405b00b95070b243d2a46e77d2c9d382
Unknown <unknown@example.com>
2020-10-01 14:55:23 +0500

    Initial commit

---
+++ Таблица_значений_функции/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Таблица_значений_функции
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
